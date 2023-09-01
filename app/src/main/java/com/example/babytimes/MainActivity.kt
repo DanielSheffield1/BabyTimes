adc4d69c02d1ba54b6513a678fbe1e1c412afa59
D-Sheffield <35811501+D-Sheffield@users.noreply.github.com>
2023-08-31 17:10:38 -0700

    Initial commit

---
+++ app/src/main/java/com/example/babytimes/MainActivity.kt
@@ -0,0 +1,22 @@
+/*
+Hello World!
+Add your name here so we know you can collaborate with the project successfully:
+
+Daniel Sheffield
+
+ */
+
+
+
+
+package com.example.babytimes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
