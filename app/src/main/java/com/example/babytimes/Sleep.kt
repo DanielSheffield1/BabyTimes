b58f90864b81f82d6b74804549456b511de4be24
12dsheffield <129481263+12dsheffield@users.noreply.github.com>
2023-11-25 14:23:34 -0800

    Accepts values for Sleep page. subtracts Time Asleep from Wake up time and adds it to the last column of the chart. Is not using databse for this function; just stores in volitile in-code variable

--- app/src/main/java/com/example/babytimes/Sleep.kt
+++ app/src/main/java/com/example/babytimes/Sleep.kt
@@ -1,18 +1,26 @@
 package com.example.babytimes
 
-import android.content.Intent;
-import android.view.View;
-import android.widget.Button;
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.service.autofill.Dataset
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.data.ChartData
+import com.github.mikephil.charting.data.DataSet
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 
-import android.os.Bundle;
-import android.widget.TextView
-import androidx.core.content.ContextCompat.startActivity
+
 
 class Sleep : AppCompatActivity() {
 
@@ -20,42 +28,92 @@ class Sleep : AppCompatActivity() {
     lateinit var barData: BarData
     lateinit var barDataSet: BarDataSet
     lateinit var barEntriesList: ArrayList<BarEntry>
-    var days = arrayOf("Sunday", "Monday", "Tuesday", "Thursday", "Friday", "Saturday")
-
+    var days = arrayOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+    lateinit var button: Button
+    public var fridayHrs = 0.0
+    lateinit var timeAsleep:EditText
+    lateinit var timeWakeUp:EditText
+    var notEmpty: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sleep)
-        barChart = findViewById(R.id.sleepChart)
-        getBarChartData()
-        barDataSet = BarDataSet(barEntriesList, "Bar Chart Data")
-        barData = BarData(barDataSet)
-        barChart.data = barData
-        var xAxis = barChart.xAxis
-        xAxis.valueFormatter = IndexAxisValueFormatter(days)
-        barDataSet.setColor(resources.getColor(R.color.purple_200))
-        barDataSet.valueTextSize = 16f
-        barChart.description.isEnabled = false
+        getBarChartData(fridayHrs)
+
 
 
     }
+    fun addSleepData(view: View?) {
+        timeAsleep = findViewById(R.id.editTextTime3)
+        timeWakeUp = findViewById(R.id.editTextTime2)
+        var numAsleep = timeAsleep.text.toString().toFloat()
+        var numWakeUp = timeWakeUp.text.toString().toFloat()
+        val result = numWakeUp - numAsleep
 
+        fridayHrs = fridayHrs.plus(result)
+        /*//old check to see if accepting edit text and performing arithmetic was working correctly
+        val myToast =
+            Toast.makeText(applicationContext, "fridayHrs = " + fridayHrs.toString(), Toast.LENGTH_SHORT)
+        myToast.setGravity(Gravity.LEFT, 200, 200)
+        myToast.show()*/
+        //redisplay the bar chart with the added sleep time
+        getBarChartData(fridayHrs)
+    }
     //Click listener that will navigate to main menu page when button (from layout) is clicked. Also requires activity entry in manifest
     fun mainMenu(view: View) {
         val intent = Intent(this, MainMenu::class.java)
         startActivity(intent)
     }
-        private fun getBarChartData() {
-            barEntriesList = ArrayList()
 
-            // on below line we are adding data
-            // to our bar entries list
-            barEntriesList.add(BarEntry(1f, 1f))
-            barEntriesList.add(BarEntry(2f, 2f))
-            barEntriesList.add(BarEntry(3f, 3f))
-            barEntriesList.add(BarEntry(4f, 4f))
-            barEntriesList.add(BarEntry(5f, 5f))
+    private fun getBarChartData(temp: Double) {
 
+        val monday = 1
+        val tuesday = 2
+        val wednesday = 3
+        val thursday = 4
+        val friday = 5
+        val mondayHrs = 9
+        val tuesdayHrs = 9.5
+        val wednesdayHrs = 11
+        val thursdayHrs = 10
+
+        barEntriesList = ArrayList()
+
+        // on below line we are adding data
+        // to our bar entries list
+        if (notEmpty)
+        {
+    barDataSet.removeLast()
+            barDataSet.removeLast()
+            val myToast =
+                Toast.makeText(applicationContext, "temp = " + temp.toString(), Toast.LENGTH_SHORT)
+            myToast.setGravity(Gravity.LEFT, 200, 200)
+            myToast.show()
+            barData.notifyDataChanged();
+            barChart.notifyDataSetChanged();
+            barChart.invalidate();
         }
+        barEntriesList.add(BarEntry(monday.toFloat(), mondayHrs.toFloat()))
+        barEntriesList.add(BarEntry(tuesday.toFloat(), tuesdayHrs.toFloat()))
+        barEntriesList.add(BarEntry(wednesday.toFloat(), wednesdayHrs.toFloat()))
+        barEntriesList.add(BarEntry(thursday.toFloat(), thursdayHrs.toFloat()))
+        barEntriesList.add(BarEntry(friday.toFloat(), temp.toFloat()))
+
+
+        barChart = findViewById(R.id.sleepChart)
+        barDataSet = BarDataSet(barEntriesList, "Hours of Sleep")
+        barData = BarData(barDataSet)
+        barChart.data = barData
+        var xAxis = barChart.xAxis
+        xAxis.valueFormatter = IndexAxisValueFormatter(days)
+        barDataSet.setColor(Color.parseColor("#22B14C"))
+        barDataSet.valueTextSize = 16f
+        barChart.description.isEnabled = false
+        notEmpty = true
 
     }
+
+
+}
+
+
